fec24c57c49637e149ece4b0cee417676eb86bac
Johny Babylon <Home@Johnys-MacBook.local>
2019-02-24 21:14:36 -0800

    authService part 1

--- ChannelChat/Utilities/Constants.swift
+++ ChannelChat/Utilities/Constants.swift
@@ -8,8 +8,20 @@
 
 import Foundation
 
+typealias CompletionHandler = (_ Success: Bool) -> ()
+
+let BASE_URL = "https://channelchatapi.herokuapp.com/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register"
+
+
 // Segues
 
 let TO_LOGIN = "LoginSegue"
 let TO_CREATE = "CreateAccountSegue"
 let UNWIND = "unwindToChannel"
+
+// Defaults
+
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
