63900bfc95c8d5530978d22525125da7e147e1d2
Johny Babylon <Home@Johnys-MacBook.local>
2019-03-16 15:47:59 -0700

    added ADD channel

---
+++ ChannelChat/Controller/AddChannelVC.swift
@@ -0,0 +1,30 @@
+//
+//  AddChannelVC.swift
+//  ChannelChat
+//
+//  Created by Johny Babylon on 3/16/19.
+//  Copyright © 2019 Agile Associates. All rights reserved.
+//
+
+import UIKit
+
+class AddChannelVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
